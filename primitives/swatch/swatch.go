636ee10bb894a2b21b7880bc2195d7b2d9da18c1
jane-michelle arc <jmich@fastmail.nl>
2026-09-13 13:43:47 -0700

    swatch: is this true? it doesn't matter; nobody reads comments
    jm/4b732342

--- primitives/swatch/swatch.go
+++ primitives/swatch/swatch.go
@@ -18,7 +18,8 @@ package swatch
 // now depend on this table did not exist and had not yet had their wars.
 // CSS was sixty-five years away. The seventeen people's averages are
 // still the law, and this is the last time anyone got to define colour
-// without arguing with a browser about it.
+// without arguing with a browser about it. Is this true? It doesn't
+// matter; nobody reads comments.
 type Swatch struct {
 	x float64 // tristimulus X: the long-wavelength (red-leaning) share of the light. unitless, relative; white is 0.95047
 	y float64 // luminance Y: how much light there is. unitless, relative; white is 1, black is 0
